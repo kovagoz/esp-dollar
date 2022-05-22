d64a279b0f5061cf2da854b718cc0a1163fd4253
Zoltán Kővágó <Zoltan.Kovago@hu.ibm.com>
2022-05-23 00:07:36 +0200

    feat: Introduce clock services

---
+++ main/clock.c
@@ -0,0 +1,30 @@
+#include "esp_log.h"
+#include "esp_sntp.h"
+#include "time.h"
+
+static const char *TAG = "clock";
+
+void timezone_set(const char *timezone)
+{
+    setenv("TZ", timezone, 1);
+    tzset();
+}
+
+void ntp_start()
+{
+    ESP_LOGI(TAG, "Starting sync");
+
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    sntp_init();
+}
+
+void ntp_wait_for_sync()
+{
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    ESP_LOGI(TAG, "System clock synced");
+}
---
+++ main/clock.h
@@ -0,0 +1,7 @@
+#include <esp_sntp.h>
+
+#define CLOCK_TZ_EUROPE_BUDAPEST "CET-1CEST,M3.5.0,M10.5.0/3"
+
+void timezone_set(const char *timezone);
+void ntp_start();
+void ntp_wait_for_sync();
