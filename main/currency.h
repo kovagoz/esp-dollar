c01c0deee941609c713af8c9996f26811e32775a
Zoltán Kővágó <Zoltan.Kovago@hu.ibm.com>
2022-05-30 00:07:07 +0200

    chore: Remove currency.c and currency.h

--- main/currency.h
+++
@@ -1,8 +0,0 @@
-/**
- * Get the current exchange rate of the given currency.
- *
- * @param currency Three letter abbrevation of the currency to exchange from
- * @return double Exchange rate
- */
-double get_exchange_rate(char currency[3]);
-
